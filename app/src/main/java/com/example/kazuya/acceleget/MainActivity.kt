689e80a75ebea3db4f5dabdae2e0c87fc7b75ec8
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-09 23:06:08 +0900

    センサーの利用

--- app/src/main/java/com/example/kazuya/acceleget/MainActivity.kt
+++ app/src/main/java/com/example/kazuya/acceleget/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.kazuya.acceleget
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.hardware.SensorManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() ,SensorEventListener{
     override fun onCreate(savedInstanceState: Bundle?) {
+        if(event == null)return
+        if(event.sensor.type == Sensor.TYPE_ACCELEROMETER){
+            txt01.text = "X軸:${event.value[0]}/nY軸:${event.values[1]}/nZ軸:${event.values[2]}"
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
