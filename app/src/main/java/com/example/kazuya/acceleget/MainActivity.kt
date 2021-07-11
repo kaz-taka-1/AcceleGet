60e42bf99cf4ca8eb2d4b17caafdb35de8f02477
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-11 23:22:55 +0900

    センサーの利用2

--- app/src/main/java/com/example/kazuya/acceleget/MainActivity.kt
+++ app/src/main/java/com/example/kazuya/acceleget/MainActivity.kt
@@ -10,20 +10,30 @@ import android.hardware.SensorManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() ,SensorEventListener{
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
         if(event == null)return
         if(event.sensor.type == Sensor.TYPE_ACCELEROMETER){
             txt01.text = "X軸:${event.value[0]}/nY軸:${event.values[1]}/nZ軸:${event.values[2]}"
         }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        TODO("Not yet implemented")
+    override fun onResume(){
+        super.onResume()
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    override fun onPause(){
+        super.onPause()
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.unregisterListener(this)
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("Not yet implemented")
-    }
 }
